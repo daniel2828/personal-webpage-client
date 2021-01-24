71bae6545730ed026d9bc260757e15a90ae16656
Daniel Tendero García <danieltendero@hotmail.es>
2021-01-24 19:57:10 +0100

    Added courses to CoursesList
    Added courses to CoursesList

--- src/components/Web/Courses/CoursesList/CourseList.js
+++ src/components/Web/Courses/CoursesList/CourseList.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./CourseList.scss";
-export default function CourseList() {
+export default function CourseList(props) {
+    const { courses } = props;
+    console.log(courses);
     return (
         <div>
             <h2>Course List</h2>
--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -5,14 +5,32 @@ import { getCoursesApi } from "../api/course";
 import PresentationCourses from "../components/Web/Courses/PresentationCourses";
 import CourseList from "../components/Web/Courses/CoursesList";
 export default function Courses() {
-   
+    const [courses, setCourses] = useState(null);
+    console.log(courses);
+    useEffect(() => {
+        getCoursesApi().then(response => {
+            if (response?.code !==200) { 
+                notification["warning"]({message:response.message})
+            } else {
+                setCourses(response.courses);
+            }
+        }).catch(() => { 
+            notification["error"]({ message: "Error del servidor, intentelo más tarde." });
+        })
+     
+    }, [])
     return (<Row>
         <Col md={4}>
             
         </Col>
         <Col md={16}>
             <PresentationCourses></PresentationCourses>
-            <CourseList></CourseList>
+            {!courses ? (
+                <Spin tip="Cargando cursos" style={{textAlign:"center", width:"100%", padding: "20px"}}></Spin>
+            ) : (
+                    <CourseList courses={ courses}></CourseList>
+            )}
+           
         </Col>
         <Col md={ 4}></Col>
     </Row>
