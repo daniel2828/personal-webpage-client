45c588069697df8b5b4f805eb2ad355337fb3f6c
Daniel Tendero García <danieltendero@hotmail.es>
2021-01-28 23:45:36 +0100

    Update AddEditPostForm.js

--- src/components/Admin/Blog/AddEditPostForm/AddEditPostForm.js
+++ src/components/Admin/Blog/AddEditPostForm/AddEditPostForm.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Row, Col, Form, Input, Button, DatePicker, notification } from "antd";
 import { Editor } from "@tinymce/tinymce-react";
 import "./AddEditPostForm.scss";
+import moment from "moment";
+import { getAccessTokenApi } from "../../../../api/auth";
 export default function AddEditPostForm() {
     return (
         <div className="add-edit-post-form">
