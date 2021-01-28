d679cbc65e456fda5f3bb3e28b50648c960845d4
Daniel Tendero García <danieltendero@hotmail.es>
2021-01-29 00:42:25 +0100

    Update AddEditPostForm.js

--- src/components/Admin/Blog/AddEditPostForm/AddEditPostForm.js
+++ src/components/Admin/Blog/AddEditPostForm/AddEditPostForm.js
@@ -4,10 +4,33 @@ import { Editor } from "@tinymce/tinymce-react";
 import "./AddEditPostForm.scss";
 import moment from "moment";
 import { getAccessTokenApi } from "../../../../api/auth";
-export default function AddEditPostForm() {
+export default function AddEditPostForm(props) {
+    const { setIsVisibleModal, setReloadPosts, post } = props;
+    const [postData, setPostData] = useState({});
+    useEffect(() => {
+        if (post) {
+            setPostData(post);
+        } else { 
+            setPostData({});
+        }
+    }, [post])
     return (
         <div className="add-edit-post-form">
-            <h1>AddEDITPOSTFORM...</h1>
+            <h1><AddEditForm postData={postData} setPostData={setPostData} post={ post}/></h1>
         </div>
+    )
+}
+
+function AddEditForm(props) { 
+    const { postData, setPostData, post} = props;
+    return (
+        <Form className="add-edit-post-form"
+            layout="inline">
+            <Row gutter={ 24}>
+                <Col span={8}></Col>
+                 <Col span={ 8}></Col>
+                  <Col span={ 8}></Col>
+            </Row>  
+        </Form>
     )
 }
