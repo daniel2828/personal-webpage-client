8a0a94dd7e73af32707039cdb5465ea95913d59b
Daniel Tendero García <danieltendero@hotmail.es>
2021-02-01 22:43:35 +0100

    Added posts and tiny editor

--- src/api/post.js
+++ src/api/post.js
@@ -33,3 +33,66 @@ export function deletePostApi(token, id){
             return err;
         })
 }
+
+export function addPostApi(token, post) {
+  const url = `${BASE_PATH}/${apiVersion}/add-post`;
+
+  const params = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token
+    },
+    body: JSON.stringify(post)
+  };
+
+  return fetch(url, params)
+    .then(response => {
+      return response.json();
+    })
+    .then(result => {
+      return result;
+    })
+    .catch(err => {
+      return err;
+    });
+}
+
+export function updatePostApi(token, id, data) {
+  const url = `${BASE_PATH}/${apiVersion}/update-post/${id}`;
+
+  const params = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token
+    },
+    body: JSON.stringify(data)
+  };
+
+  return fetch(url, params)
+    .then(response => {
+      return response.json();
+    })
+    .then(result => {
+      return result;
+    })
+    .catch(err => {
+      return err;
+    });
+}
+
+export function getPostApi(urlPost) {
+  const url = `${BASE_PATH}/${apiVersion}/get-post/${urlPost}`;
+
+  return fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(result => {
+      return result;
+    })
+    .catch(err => {
+      return err;
+    });
+}
